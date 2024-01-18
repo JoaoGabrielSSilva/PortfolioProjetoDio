3d278aa912b498401da20475bc7f73f2a5b80f79
João Gabriel Souza Silva <90883086+JoaoGabrielSSilva@users.noreply.github.com>
2024-01-18 15:52:17 -0300

    colocando a url dos dados do json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData(){
-    const url = ''
+    const url = 'https://raw.githubusercontent.com/JoaoGabrielSSilva/Portfolio/main/data/profile.json'
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
